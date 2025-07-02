5fe9e0f8c18e9d9cc1f4ba74a3fe32a36002bc3b
erhanbilir <eturanbilir@outlook.com>
2025-07-02 13:34:47 +0300

    Remove ignored build folders

--- Debug/Src/main.d
+++
@@ -1,9 +0,0 @@
-Src/main.o: ../Src/main.c \
- C:/Users/erhan/STM32CubeIDE/workspace_f407/driver_development_f407/myDrivers/Inc/stm32f407xx.h \
- C:/Users/erhan/STM32CubeIDE/workspace_f407/driver_development_f407/myDrivers/Inc/RCC.h \
- C:/Users/erhan/STM32CubeIDE/workspace_f407/driver_development_f407/myDrivers/Inc/stm32f407xx.h \
- C:/Users/erhan/STM32CubeIDE/workspace_f407/driver_development_f407/myDrivers/Inc/GPIO.h
-C:/Users/erhan/STM32CubeIDE/workspace_f407/driver_development_f407/myDrivers/Inc/stm32f407xx.h:
-C:/Users/erhan/STM32CubeIDE/workspace_f407/driver_development_f407/myDrivers/Inc/RCC.h:
-C:/Users/erhan/STM32CubeIDE/workspace_f407/driver_development_f407/myDrivers/Inc/stm32f407xx.h:
-C:/Users/erhan/STM32CubeIDE/workspace_f407/driver_development_f407/myDrivers/Inc/GPIO.h:
--- Debug/myDrivers/Src/GPIO.d
+++
@@ -1,9 +0,0 @@
-myDrivers/Src/GPIO.o: ../myDrivers/Src/GPIO.c \
- C:/Users/erhan/STM32CubeIDE/workspace_f407/driver_development_f407/myDrivers/Inc/GPIO.h \
- C:/Users/erhan/STM32CubeIDE/workspace_f407/driver_development_f407/myDrivers/Inc/stm32f407xx.h \
- C:/Users/erhan/STM32CubeIDE/workspace_f407/driver_development_f407/myDrivers/Inc/RCC.h \
- C:/Users/erhan/STM32CubeIDE/workspace_f407/driver_development_f407/myDrivers/Inc/GPIO.h
-C:/Users/erhan/STM32CubeIDE/workspace_f407/driver_development_f407/myDrivers/Inc/GPIO.h:
-C:/Users/erhan/STM32CubeIDE/workspace_f407/driver_development_f407/myDrivers/Inc/stm32f407xx.h:
-C:/Users/erhan/STM32CubeIDE/workspace_f407/driver_development_f407/myDrivers/Inc/RCC.h:
-C:/Users/erhan/STM32CubeIDE/workspace_f407/driver_development_f407/myDrivers/Inc/GPIO.h:
